0ae6d8b578db4db280a10371704a262e6d0dd908
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-02-23 16:04:50 -0600

    Ya solo me falta el boton :v y arreglar un bugaso con el zindex
    hola

--- resources/views/Producciones/producciones.blade.php
+++ resources/views/Producciones/producciones.blade.php
@@ -7,16 +7,16 @@
 {{-- PHOTOSWIPE --}}
 
 <!-- Core CSS file -->
-<link rel="stylesheet" href="{{ asset('/plugins/PhotoSwipe/dist/photoswipe.css') }}"> 
+<link rel="stylesheet" href="{{ asset('/plugins/PhotoSwipe/dist/photoswipe.css') }}">
 
 <!-- Skin CSS file (styling of UI - buttons, caption, etc.)
     In the folder of skin CSS file there are also:
-    - .png and .svg icons sprite, 
+    - .png and .svg icons sprite,
     - preloader.gif (for browsers that do not support CSS animations) -->
-<link rel="stylesheet" href="{{ asset('/plugins/PhotoSwipe/dist/default-skin/default-skin.css') }}"> 
+<link rel="stylesheet" href="{{ asset('/plugins/PhotoSwipe/dist/default-skin/default-skin.css') }}">
 
 <!-- Core JS file -->
-<script src="{{ asset('/plugins/PhotoSwipe/dist/photoswipe.min.js') }}"></script> 
+<script src="{{ asset('/plugins/PhotoSwipe/dist/photoswipe.min.js') }}"></script>
 
 <!-- UI JS file -->
 <script src="{{ asset('/plugins/PhotoSwipe/dist/photoswipe-ui-default.min.js') }}"></script>
@@ -51,18 +51,23 @@
                         <img id="gal_high" class="gal_high" src="{{ asset('/img/fotoZinnia/Procella.png') }}" alt="">
                     </div>
                 </div>
-                <div class="uk-width-1 gal_desc font_vietnam" uk-tooltip="title: Descripción de la foto, Lorem ipsum dolor sit aawdawdawd adw dadwa awdawdawdawd awdawdad awdawd awd awd awdaw dawdawd met, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.d">Descripción de la foto, Lorem ipsum dolor sit aawdawdawd adw dadwa awdawdawdawd awdawdad awdawd awd awd awdaw dawdawd met, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.</div>
-            <div class="font_vietnam uk-margin-top uk-width-1 uk-text-center" style="font-size: 22px; color:#FFFFFF;">GALERIA DE FOTOS</div>
-            <div class="font_vietnam uk-margin-small-top uk-width-1 uk-text-center" style="font-size: 14px;color: #FFFFFF;">15 fotos para ver</div>
+                <div class="uk-width-1 gal_desc font_vietnam"
+                    uk-tooltip="title: Descripción de la foto, Lorem ipsum dolor sit aawdawdawd adw dadwa awdawdawdawd awdawdad awdawd awd awd awdaw dawdawd met, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.d">
+                    Descripción de la foto, Lorem ipsum dolor sit aawdawdawd adw dadwa awdawdawdawd awdawdad awdawd awd
+                    awd awdaw dawdawd met, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore
+                    magna aliqua.</div>
+                <div class="font_vietnam uk-margin-top uk-width-1 uk-text-center"
+                    style="font-size: 22px; color:#FFFFFF;">GALERIA DE FOTOS</div>
+                <div class="font_vietnam uk-margin-small-top uk-width-1 uk-text-center"
+                    style="font-size: 14px;color: #FFFFFF;">15 fotos para ver</div>
             </div>
             <div class="uk-width-1-2@m">
             </div>
         </div>
     </div>
-        
-    <div class="uk-container">
 
-        <div class="uk-width-1-1 uk-margin-top uk-margin-bottom">
+    <div class="uk-container  uk-container-xlarge">
+        <div class="uk-width-1-1">
             <div class=" uk-grid-match" uk-grid>
                 <div class="uk-width-1-3@m">
                     <div class="uk-card more-info">
@@ -113,6 +118,45 @@
 
         </div>
     </div>
+
+    <div class="uk-section uk-section-small">
+        <div class="uk-width-1 div-reconocimientos uk-text-center uk-padding">
+            <h3 class="uk-text-center">RECONOCIMIENTOS</h3>
+            <h4 class="uk-text-center">Galardonada a n reconocimientos</h4>
+            <p>*Obra seleccionada para participar en la Muestra Estatal de Teatro Michoacán 2021/ Ganadora de la
+                Convocatoria Sala Virtual 2021 Voy al Teatro, Jalisco/ Obra seleccionada para ser parte del Banco de
+                Proyectos de CREADORES DE PAZ del Centro Occidente/ Proyecto seleccionado en la convocatoria México
+                Encuentro de las Artes Escénicas (ENARTES) FONCA 2020, Obra seleccionada en el Ciclo Teatro de los
+                Estados 2020, Teatro La Capilla, CDMX/ Producción seleccionada en el Programa Cultura de Paz dentro del
+                proyecto Promoción del Teatro en Michoacán 2019/ Proyecto beneficiado por el Programa de Estímulos a la
+                Creación y Desarrollo Artístico de Michoacán (PECDAM 2018) Gira por el interior del Estado de Michoacán
+                en el rubro de Difusión de las Artes Escénicas.</p>
+        </div>
+    </div>
+
+    <div class="uk-container  uk-container-xlarge">
+        <div class="uk-width-1-1">
+            <div class=" uk-grid-match" uk-grid>
+                <div class="uk-width-3-5@m">
+                    <div class="uk-card more-info">
+                        <h1>CRÍTICAS</h1>
+                        <p>"Una obra impresionante que hace llorar a cualquiera"
+                            José Agustín Solórzano, El Universal. </p>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquid ex ea commodi consequat."
+                            José Agustín Solórzano, El Universal. </p>
+                    </div>
+                </div>
+                <div class="uk-width-expand@m">
+                    <div class="uk-card">
+                        hola
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
 
 <script>
@@ -153,14 +197,14 @@
 <!-- Root element of PhotoSwipe. Must have class pswp. -->
 <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
 
-    <!-- Background of PhotoSwipe. 
+    <!-- Background of PhotoSwipe.
         It's a separate element as animating opacity is faster than rgba(). -->
     <div class="pswp__bg"></div>
 
     <!-- Slides wrapper with overflow:hidden. -->
     <div class="pswp__scroll-wrap">
 
-        <!-- Container that holds slides. 
+        <!-- Container that holds slides.
             PhotoSwipe keeps only 3 of them in the DOM to save memory.
             Don't modify these 3 pswp__item elements, data is added later on. -->
         <div class="pswp__container">
@@ -190,15 +234,15 @@
                 <!-- element will get class pswp__preloader--active when preloader is running -->
                 <div class="pswp__preloader">
                     <div class="pswp__preloader__icn">
-                    <div class="pswp__preloader__cut">
-                        <div class="pswp__preloader__donut"></div>
-                    </div>
+                        <div class="pswp__preloader__cut">
+                            <div class="pswp__preloader__donut"></div>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                <div class="pswp__share-tooltip"></div> 
+                <div class="pswp__share-tooltip"></div>
             </div>
 
             <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
@@ -278,7 +322,7 @@
 
         gallery.listen('gettingData', function(index, item) {
             if (item.w < 1 || item.h < 1) { // unknown size
-                var img = new Image(); 
+                var img = new Image();
                 img.onload = function() { // will get size after load
                     item.w = this.width; // set image width
                     item.h = this.height; // set image height
@@ -288,7 +332,7 @@
                 img.src = item.src; // let's download image
             }
         });
-        
+
         // Gallery starts closing
         gallery.listen('afterChange', function() {
             setIndex(gallery.getCurrentIndex());
