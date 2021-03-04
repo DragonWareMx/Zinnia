e9dde4c14c157ceee03bdeccb991ff1ebc1209cd
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-03-04 08:29:48 -0600

    footer arreglao
    creo

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -16,33 +16,32 @@ class EventSeeder extends Seeder
     {
         //
         DB::table('events')->insert([
-            'titulo'=>'LA PRUDENCIA',
-            'fecha'=>'2021-03-21',
-            'hora'=>'18:00',
-            'lugar'=>'',
-            'tipo'=>'Presentación virtual',
-            'link'=>'https://www.youtube.com/channel/UCcVtQc1KVtHPUhHNBZmXvfw',
-            'masinfo'=>'Muestra Estatal de Teatro Michoacán MET 2020',
+            'titulo' => 'LA PRUDENCIA',
+            'fecha' => '2021-03-21',
+            'hora' => '18:00',
+            'lugar' => '',
+            'tipo' => 'Presentación virtual',
+            'link' => 'https://www.youtube.com/channel/UCcVtQc1KVtHPUhHNBZmXvfw',
+            'masinfo' => 'Muestra Estatal de Teatro Michoacán MET 2020',
         ]);
         DB::table('events')->insert([
-            'titulo'=>'Conversatorio/Desmontaje:  DESMONTAJE DE PROCESOS ARTÍSTICOS Y VIRTUALIDAD/LA PRUDENCIA',
-            'fecha'=>'2021-03-21',
-            'hora'=>'19:15',
-            'lugar'=>'',
-            'tipo'=>'Presentación virtual',
-            'link'=> 'https://www.youtube.com/channel/UCcVtQc1KVtHPUhHNBZmXvfw',
-            'masinfo'=>'Muestra Estatal de Teatro MET 2020',
+            'titulo' => 'Conversatorio/Desmontaje:  DESMONTAJE DE PROCESOS ARTÍSTICOS Y VIRTUALIDAD/LA PRUDENCIA',
+            'fecha' => '2021-03-21',
+            'hora' => '19:15',
+            'lugar' => '',
+            'tipo' => 'Presentación virtual',
+            'link' => 'https://www.youtube.com/channel/UCcVtQc1KVtHPUhHNBZmXvfw',
+            'masinfo' => 'Muestra Estatal de Teatro MET 2020',
         ]);
 
         DB::table('events')->insert([
-            'titulo'=>'LA PRUDENCIA',
-            'fecha'=>'',
-            'hora'=>'19:15',
-            'lugar'=>'',
-            'tipo'=>'Presentación virtual',
-            'link'=>'https://voyalteatro.com/ ',
-            'masinfo'=>'Temporada en Sala Virtual 2021 Voy al Teatro, Jalisco',
+            'titulo' => 'LA PRUDENCIA',
+            'fecha' => '2021-03-21',
+            'hora' => '19:15',
+            'lugar' => '',
+            'tipo' => 'Presentación virtual',
+            'link' => 'https://voyalteatro.com/ ',
+            'masinfo' => 'Temporada en Sala Virtual 2021 Voy al Teatro, Jalisco',
         ]);
-
     }
 }
